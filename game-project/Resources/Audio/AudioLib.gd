fd8f131beea5f5ab64ba6c4e0e7e73bafa83152c
Joona Eskelinen <joona.eskelinen04@gmail.com>
2026-03-19 21:52:28 +0200

    chore: resources + audiofile
    added sprites, audio, a font, dialogues and a global gd script to hold audio

---
+++ game-project/Resources/Audio/AudioLib.gd
@@ -0,0 +1,24 @@
+extends Node
+
+const MUSIC = {
+	"camp_1" = preload("res://Resources/Audio/Songs/CozyGameDigSiteVersionMaybe.mp3"),
+	"main" = preload("res://Resources/Audio/Songs/CozyGameRoughSketch01.mp3"),
+}
+
+const AMBIANCE = {
+	"camp_1" = preload("res://Resources/Audio/Ambiance/GameProject2_DigSiteAmbience.mp3"),
+	
+}
+
+const SFX = {
+	"BrushSound" = preload("res://Resources/Audio/SoundEffects/GameProject2_BRUSHSOUNDS.wav"),
+	"DigSound" = preload("res://Resources/Audio/SoundEffects/GameProject2_DiggingSoundz2.wav"),
+	"DoorExit" = preload("res://Resources/Audio/SoundEffects/GameProject2_DOORsound1(exitRoom).wav"),
+	"DoorEnter" = preload("res://Resources/Audio/SoundEffects/GameProject2_DOORsound2(enterNewArea).wav"),
+	"MenuClick" = preload("res://Resources/Audio/SoundEffects/GameProject2_MENU_CLICK.wav"),
+	"MenuNotif" = preload("res://Resources/Audio/SoundEffects/GameProject2_MENU_NOTIFICATION.wav"),
+	"WalkConcrete" = preload("res://Resources/Audio/SoundEffects/GameProject2_WalkingOnConcreteNWood.wav"),
+	"WalkDirt" = preload("res://Resources/Audio/SoundEffects/GameProject2_WalkingOnGRASSnDIRT.wav"),
+	"WalkGeneral" = preload("res://Resources/Audio/SoundEffects/GameProject2_WalkingSoundz.wav"),
+	
+}
