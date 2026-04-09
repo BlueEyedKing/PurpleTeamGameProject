71ffbcf9807091c143bfc9c176ef5d9de300b180
Joona Eskelinen <joona.eskelinen04@gmail.com>
2026-04-09 15:38:38 +0300

    Feat: NPC spawn location system

--- game-project/Scripts/NPC.gd
+++ game-project/Scripts/NPC.gd
@@ -6,6 +6,22 @@ class_name NPC
 
 func _ready() -> void:
 	interaction_area.interact = Callable(self, "_on_interact")
-
+	_apply_placement()
+	
 func _on_interact():
 	DialogueUi.start_for_npc(npc_id)
+
+func _apply_placement() -> void:
+	if npc_id == "":
+		return
+	
+	var marker_name = npc_id + "_day" + str(TimeManager.current_day)
+	var marker = get_parent().get_node_or_null(marker_name)
+	if marker:
+		global_position = marker.global_position
+		return
+	var default_marker = get_parent().get_node_or_null(npc_id + "_default")
+	if default_marker:
+		global_position = default_marker.global_position
+	else:
+		queue_free()
